c59cb5959858e60fc76a3b344b6ebdf77f57b4ac
kecapbangor <90863106+kecapbangor@users.noreply.github.com>
2026-06-29 15:34:59 +0700

    Delete PHP Database directory

--- PHP Database/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Memanggil sesi yang sedang aktif
-session_start();
-// Menghancurkan seluruh data sesi (menghapus status login)
-session_destroy();
-// Mengembalikan user ke halaman login
-header("Location: login.php");
-exit();
-?>
